1218f44e5c5cb42805659a05b6a63e1e988c030e
Siku788 <kahmoshlarka@gmail.com>
2025-05-14 11:33:20 +0500

    mul added

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ def add(a, b):
 def sub(a, b):
     return a - b
 
-
+def mul(a, b):
+    return a * b
+  
 result1 = add(4, 5)
 result2 = sub(4, 1)
+result3 = mul(4, 1)
 
-print(result1, result2)
+
+
+print(result1, result2,result3)
