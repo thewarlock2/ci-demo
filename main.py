6cc6c9876fdb96168e8eedacd5c8ee9eea3a7b08
Siku788 <kahmoshlarka@gmail.com>
2025-05-14 10:17:32 +0500

    with flake8 lint

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-def add(a,b):
-  return a+b
+def add(a, b):
+    return a + b
 
 
-def sub(a, b): return a-b
+def sub(a, b):
+    return a - b
 
 
-result1=add(4,5)
-result2=sub(4,1)
+result1 = add(4, 5)
+result2 = sub(4, 1)
 
-print(result1,result2)
+print(result1, result2)
 
